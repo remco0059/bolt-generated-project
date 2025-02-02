38571505b37381a8c46e87b11937c96598d0824d
remco0059 <remcovdgoes@gmail.com>
2025-02-02 12:50:03 +0100

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+    function Home() {
+      return (
+        <div className="container mt-5">
+          <h2>Home Page</h2>
+          <p>This is the home page of my simple website.</p>
+        </div>
+      );
+    }
+
+    export default Home;
